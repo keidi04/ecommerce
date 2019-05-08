8d9f6317a3479ad7f962a8f412c97460c9b56f47
Rafael Keidi <rafaelkeidi@Rafaels-iMac.local>
2019-05-08 19:11:14 -0300

    função delete

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -118,5 +118,14 @@ class User extends Model {
 		$this->setData($results[0]);
 
 	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+	}
 }
 ?>
