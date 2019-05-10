fa8caad70fac55dd9efd71f5d324ff07e5c7a922
Rafael Keidi <rafaelkeidi@Rafaels-iMac.local>
2019-05-10 15:28:30 -0300

    admin adição, edição e funcção deletar categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new \Slim\Slim();
 
@@ -146,6 +147,79 @@ $app->post("/admin/users/:iduser", function($iduser) {
 
 	header("location: /admin/users");
 
+	exit;
+
+});
+
+$app->get('/admin/categories' , function()  {
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+});
+$app->get('/admin/categories/create' , function()  {
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->post('/admin/categories/create' , function()  {
+
+	$category = new Category;
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory)  {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory)  {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update",[
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory)  {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
 	exit;
 
 });
